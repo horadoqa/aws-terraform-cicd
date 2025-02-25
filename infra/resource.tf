a8cb7e3aa24866d13107262b9cf587af067a379c
Ricardo Fahham <rfahham@hotmail.com>
2025-02-25 18:48:31 -0300

    Apply infra

--- infra/resource.tf
+++ infra/resource.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "k6" {
   count         = var.number_instances
   ami           = var.aws_ami
   instance_type = var.instance_type
+  user_data = file("user-data.sh")
 
   security_groups = [aws_security_group.allow_ssh_http.name]
 
   tags = {
-    Name = "k6"
+    Name = "k6-${count.index}"
   }
 }
---
+++ infra/user-data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instalar ferramentas
+sudo apt-get install git -y
+sudo apt-get install curl -y
+sudo apt-get install jq -y
+sudo apt-get install vim -y
+
+# Install k6
+sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-get update  -y
+sudo apt-get install k6
